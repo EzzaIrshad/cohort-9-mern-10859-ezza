d3c87b11d8f8c6138c862e1d79270b2feb5a6fb9
Ezza Irshad <ezzairshad19@gmail.com>
2026-07-31 22:06:23 +0500

    Fix review issues

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 // Axios instance for API calls
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
+    timeout: 5000
 })
 
-// TODO: Add interceptors for request and response handling, such as adding authorization headers or handling errors globally.
 api.interceptors.request.use(
     (config) => {
         return config;
--- frontend/src/app/routes/AppRouter.tsx
+++ frontend/src/app/routes/AppRouter.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
+import NotFound from '../../pages/NotFound'
 
 export default function AppRouter() {
     return (
         <Routes>
             <Route path="/" element={<h1>Home</h1>} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
