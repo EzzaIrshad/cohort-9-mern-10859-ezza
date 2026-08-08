9907b4e180503815d6c19e0f965322fb9b5a70af
Ezza Irshad <ezzairshad19@gmail.com>
2026-08-08 22:00:58 +0500

    Implement theme provider using shadcn

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,15 @@ import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import './index.css'
 import App from './App.tsx'
+import { ThemeProvider } from './app/providers/ThemeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryProvider>
-        <App />
+        <ThemeProvider defaultTheme='light'>
+          <App />
+        </ThemeProvider>
       </QueryProvider>
     </BrowserRouter>
   </StrictMode>,
---
+++ frontend/src/shared/components/ThemeToggle.tsx
@@ -0,0 +1,28 @@
+import { IoSunny as Sun } from "react-icons/io5";
+import { LuMoon as Moon } from "react-icons/lu";
+import { useTheme } from "../../app/providers/ThemeProvider";
+
+const ThemeToggle = () => {
+    const { theme, setTheme } = useTheme();
+
+    const handleToggle = () => {
+        setTheme(theme === "dark" ? "light" : "dark");
+    }
+    return (
+        <button
+            onClick={handleToggle}
+            aria-label="Toggle theme"
+            className="group relative size-8 sm:size-10 rounded-full glass flex items-center justify-center overflow-hidden transition-transform 
+            hover:scale-110 active:scale-95 bg-gradient-soft dark:bg-muted icon-container-shadow dark:bg-linear-0 dark:border dark:border-gray-500"
+        >
+            {
+                theme === "dark" ?
+                    <Sun className="size-3.5 sm:size-4 text-white fill-white icon-shadow transition-transform group-hover:rotate-90" />
+                    :
+                    <Moon className="size-3.5 sm:size-4 text-primary fill-primary icon-shadow transition-transform group-hover:-rotate-12" />
+            }
+        </button>
+    )
+}
+
+export default ThemeToggle
